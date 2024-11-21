1f6befed9d59a8d4bd06098dc7a992d8f62beebd
Manohar <nmanohar0707@gmail.com>
2024-11-21 19:22:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+console.log("Welcome to Freedom Tunes");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');//default song
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Vande Mataram - AR Rahaman", filePath: "songs/1.mp3", coverPath: "covers/1.jpeg"},
+    {songName: "Jai Ho - AR Rahaman", filePath: "songs/2.mp3", coverPath: "covers/2.jpeg"},
+    {songName: "Desh Mere-Arjith Singh", filePath: "songs/3.mp3", coverPath: "covers/3.jpeg"},
+    {songName: "Maa Tujhe Salaam- AR Rahaman", filePath: "songs/4.mp3", coverPath: "covers/4.jpeg"},
+    {songName: "Chak De! India - SRK", filePath: "songs/5.mp3", coverPath: "covers/5.jpeg"},
+    {songName: "Ae Watan -Arjit Singh", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Leha Ra Do - Arjith Singh", filePath: "songs/7.mp3", coverPath: "covers/7.jpeg"},
+    {songName: "Jana Gana Mana", filePath: "songs/8.mp3", coverPath: "covers/8.jpeg"},
+    {songName: "India wale", filePath: "songs/9.mp3", coverPath: "covers/9.jpeg"},
+    {songName: "Sare Jahan Se Achcha", filePath: "songs/10.mp3", coverPath: "covers/10.jpeg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
